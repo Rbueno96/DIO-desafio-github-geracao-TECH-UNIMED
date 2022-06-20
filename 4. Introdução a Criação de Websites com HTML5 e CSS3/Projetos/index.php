ca69176fe0ff1d275f55d76106115419c3bc02c3
Rbueno96 <robueno1996@gmail.com>
2022-06-20 11:19:54 -0300

    Seguindo com o curso

---
+++ 4. Introdução a Criação de Websites com HTML5 e CSS3/Projetos/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>MEU SITE!</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<header>
+			<img src="capa do readme.jpg" alt="Foto de capa do portfólio do Github">
+			<h1 id="title">Rodrigo Oliveira Bueno</h1>
+		</header>
+		
+		<section>
+			<header>
+				<h2 class="subtitle">Postagens</h2>
+			</header>
+			
+			<article class="post">
+				<header>
+				<h3 class="post_title">Post #1</h3>
+				</header>
+				<p class="post_content">
+					O Lorem Ipsum é um <a href="https://www.linkedin.com/in/rodbueno/"target="_blank"> modelo da indústria tipográfica e de impressão</a>. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham passagens com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker que incluem versões do Lorem Ipsum.
+				</p>
+			</article>
+				
+		</section>
+		<footer>
+			<ul>
+				<li>
+					<a href="mailto:robueno1996@gmail.com">robueno1996@gmail.com</a>
+				</li>
+				<li>
+					<a href="https://www.linkedin.com/in/rodbueno/"target="_blank">Meu LinkeIn</a>
+				</li>
+				<li>
+					<h3>Whatsapp</h3>
+		</footer>
+	</body>
+	
+</html>
---
+++ README.md
@@ -0,0 +1,3 @@
+# BEM-VINDO, WELCOME! :wave:
+
+![capa do readme](https://user-images.githubusercontent.com/107258826/174150452-31c8ac00-0bfa-475a-ac54-b6f4923797ab.jpg)
